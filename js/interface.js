1941fe71b7dd087c1a6197e55de3d5118b5ed2a6
domhir <119183403+domhir@users.noreply.github.com>
2025-11-10 12:46:35 +0100

    Update interface.js

--- js/interface.js
+++ js/interface.js
@@ -2,7 +2,7 @@
 let LEVEL_PERCENTS = [0.7, 0.55, 0.4, 0.25, 0.2, 0.15, 0.1, 0.05, 0];
 //let LEVEL_PERCENTS = [0];
 let LEVEL_NAMES = [
-  "Genius",
+  "You a G",
   "Exceptional",
   "Fabulous",
   "Superb",
